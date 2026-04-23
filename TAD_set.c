a442de69c4567904560113ea4bae83621fe58a65
sofimicol <sofimicol3@gmail.com>
2026-04-23 17:23:16 -0300

    Refactor set operations and add new functionalities

--- TAD_set.c
+++ TAD_set.c
@@ -1,17 +1,66 @@
 #include "TAD_set.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "TAD_str.h"
 
 int esvacio(Tdata set);
+Tdata copia_ast(Tdata elemento);
 
-Tdata create_set(){ //crea conjunto vacio
-	Tdata set; 
-	set->nodeType=2;
-	set->data=NULL;
-	set->next=NULL; 
-	return set;
+Tdata create_gen(Tdata data,int tipo){ //crea conjunto vacio
+	Tdata gen;  
+	gen=(Tdata)
+	gen->nodeType=tipo; // [2] o [3] 
+	gen->data=data; 
+	gen->next=NULL; 
+	return gen;
 }
-void insert_set(Tdata* set, Tdata elem){
+Tdata create_str(){
+	Tdata str; 
+	set->nodeType=1;
+	return str;
+}
+Tdata create_ast(){
+	Tdata nuevo;
+	int tipo;
+	printf("\nIngrese el tipo de dato que quiere ingresar: [1] str, [2] set y [3] list");
+	scanf("%d",&tipo);
+	do{ // verifica que se esten ingresando numeros dentro del rango solicitado entre 1 y 3
+		printf("\nIngrese el tipo de dato que quiere ingresar: [1] str, [2] set y [3] list");
+		scanf("%d",&tipo);
+	} while(tipo<1 && tipo>3);
+	
+	if(tipo==1){
+		nuevo=create_str(); //crea un Tdata del tipo str
+		nuevo->string=load(); //leo el str del conjunto o lista
+	}
+	else {
+		Tdata data=create_ast();
+		nuevo=create_gen(data,tipo); //crea un Tdata del tipo set o list
+	}
+	return nuevo;
+}
+
+Tdata copia_ast(Tdata elemento){ 
+	Tdata ast;
+	ast=(Tdata)malloc(sizeof(Tdata));
+	ast->nodeType=elemento->nodeType;// numero que representa el tipo de dato a almacenar 
+	ast->data=elemento->data;
+	ast->next=NULL;
+	return ast;
+}
+Tdata cargar_conjunto(){
+	Tdata arbol;
+	Tdata elem;
+	printf("\nIngrese el 0 para terminar");
+	int opcion;
+	opcion=1;
+	do{
+		elem=create_ast();
+		insert_set(&arbol,elem);
+	} while(opcion!=0);
+	
+}
+void insert_set(Tdata* set, Tdata elem){ //insertar un elemento en un conjunto sin duplicados
 	Tdata aux;
 	if(esvacio(*set)==1){
 		*set->data=elem;
@@ -21,17 +70,18 @@ void insert_set(Tdata* set, Tdata elem){
 		while(aux->next!=NULL){
 			aux=aux->next;
 		}
-		aux->next=elem;
+		aux->next=copia_ast(elem);
 	}
 }
+	
 int belongs(Tdata set, Tdata elem){ //pertenece elem es str o set-list
 	Tdata aux;
 	if(esvacio(set)==1){
 		return 0;
 	}
 	aux=set;
-	while(aux->sig!=NULL && aux->data!=elem->data){
-		aux=aux->sig;
+	while(aux->sig!=NULL && compara(aux->data,elem->data)){  // crear una funcion que sea son distintos y se pasen los data?
+		aux=aux->next;
 	}
 	if(aux->sig==NULL){
 		return 0; //elem no pertenece al conjunto, llegamos al final del conjunto
@@ -39,7 +89,7 @@ int belongs(Tdata set, Tdata elem){ //pertenece elem es str o set-list
 	else{
 		return 1; //el elem pertenece al conjunto
 	}
-}
+	
 void remove_set(Tdata* set, Tdata elem){ //elem es str o set-list
 	Tdata actual;
 	Tdata anterior=NULL;
@@ -47,7 +97,7 @@ void remove_set(Tdata* set, Tdata elem){ //elem es str o set-list
 		printf("\nEl conjunto es vacio, no se pudo eliminar elemento");
 	}else{
 		actual=*set;
-		while(actual->sig!=NULL && actual->data!=elem->data){
+		while(actual->sig!=NULL && actual->data!=elem->data){ // funcion generica
 			anterior=actual;
 			actual=actual->next;
 		}
@@ -78,10 +128,10 @@ void eliminarprimero(Tdata* set){
 Tdata union_set(Tdata A, Tdata B); 
 Tdata intersection_set(Tdata A, Tdata B); 
 Tdata difference_set(Tdata A, Tdata B); // son los elementos que pertenecen al primer conjunto y no pertenecen al segundo conjunto
-int subset(Tdata A, Tdata B){ // {1,2} {1,3,4,5,6} o {1,2,3,4,5,6} {1,2} // crear un modulo que retorne el tamaño y se comparen, A debe ser mas pequeño de B
+int subset(Tdata A, Tdata B){ // {1,2} {1,3,4,5,6} o {1,2,3,4,5,6} {1,2} // crear un modulo que retorne el tamaÃ±o y se comparen, A debe ser mas pequeÃ±o de B
 	//suponiendo que la funcion pregunta que A contiene a B
 	int ban=0;
-	Tdata auxA=auxB=NULL;
+	Tdata auxA=auxB=create_gen(2);
 	if(tamanioset(A)<tamanioset(B)){
 		auxA=A;
 		while(auxA->next!=NULL&&b==0){
@@ -105,4 +155,181 @@ int subset(Tdata A, Tdata B){ // {1,2} {1,3,4,5,6} o {1,2,3,4,5,6} {1,2} // crea
 	}
 }
 int equals_set(Tdata A, Tdata B); //retorna 1 si son iguales y 0 si no lo son
+
+
+/*-------------------------------------------------------------
+-----------------------------------------------------
+---------------------------------------------------*/
+//Operaciones para listas
+Tdata create_list(){ //crea conjunto vacio
+	Tdata list; 
+	set->nodeType=3;
+	set->data=NULL;
+	set->next=NULL; 
+	return list;
+}
+
+// Objetivo append (Conentar un elemento al ultimo de la lista)	
+// Condiciones (Modificar Listas originales)
+// Uso de referencias (Para desplazarse) - (Para guardar datos del nodo) - (Para encontrar el anteultimo nodo) - (Para enlazar nodos)
+	
+	Tdata append(Tdata* L1, Tdata elemento) { //cargar una lista que dentro puede tener datos str, set o list
+		Tata aux = *L1; // Referencia para desplazamiento
+		Tdata nuevo = create_ast(3); // Nuevo nodo para cargar elemento
+		
+		///nuevo->nodoType = // Elemento generico
+		
+		///copy_str(nuevo->data, elemento); // Berificar argumentos que recibe copy_str
+		
+		if (*(L1 == NULL) {
+			*L1 = nuevo;
+			return NULL;
+		}else {
+			while (aux->next != NULL) { // Desplazamiento para obtener posicion del anteultimo nodo
+				aux = aux->next;
+			}
+			
+			aux->next = nuevo; // Cargo el elemento al final de la lisa
+		}
+	}
+	
+int length(Tdata list) { // Calcula el tamanio de lalista
+	int c = 0; // Contador
+	Tdata aux = list; // Auxiliar (opcional) para despalzarce
+	
+	while(aux != NULL) { // Desplazamiento
+		c++;
+		aux = aux->next;
+	}
+	
+	return c; // Tamanio de la lista
+}
+
+// Objetivo de concatenar (Conentar ultimo nodo de L1 con L2)
+// Condiciones (Conservar Listas originales) - (Retornar una nueva Lista)
+// Uso de referencias (Para desplazarse) - (Para guardar datos del nodo) - (Para encontrar el ultimo nodo) - (Para enlazar nodos)
+
+Tdata concat(Tdata L1, Tdata L2) { 
+	Tdata resultado;
+	Tdata fin; // referencia a ultimo nodo de L1
+	Tdata aux; // nodo aux para recorrer L2
+	
+	if (L1 == NULL && L2 == NULL) { // posible funcion esVacio
+		return NULL;
+	}else{
+		resultado = copy_list(L1); // No se mocifica L1 se guarda en resultado
+		fin = resultado; // referencia a ultimo nodo de L1
+		
+		while(fin->next != NULL) { // busco el ultimo de L1
+			fin = fin->next;
+		}
+	}
+	
+	aux = L2; // Referencia para desplazarce sobre L2 y copiar datos de L2 a resultado
+	
+	while(aux != NULL) {
+		Tdata nuevo = (Tdata)malloc(sizeof(Tdata)); // Creo nuevo nodo para guardar datos de L2
+		
+		//Berificar argumentos que recibe copy_str
+		///copy_str(nuevo->data, aux->data); // Funcion que reeplazaria a strcpy de libreria strin.h
+		
+		// Concateno ultimo nodo de L1 con el nodo de L2
+		fin->next = nuevo;
+		fin = nuevo;
+		
+		aux = aux->next; // Muevo referencia al siguiente
+	}
+	return resultado;
+}
+/*-------------------------------------------
+------------------------------------------------
+-----------------------------------------------*/
+
+Tdata copy_set(Tdata A) {
+	Tdata nuevo;
+	while(B!=NULL){
+		insert_set(&nuevo,B->data);
+	}
+	return nuevo;
+}
+
+Tdata union_set(Tdata A, Tdata B) {
+	Tdata resultado; //creategen
+	Tdata aux = B; 
+	Tdata nuevo;
+	resultado=copy_set(A); 	
+	Tdata fin = resultado;
+	while(fin != NULL) {
+		fin = fin->next;
+	}
+		while(aux != NULL) {
+			if (belong(fin->data, aux->data)==0) {
+				
+				nuevo = create_gen(null,2); //no esta
+				nuevo->data=copia_ast(aux->data);
+				
+				nuevo->next = fin;
+				fin = nuevo;
+			}
+			aux = aux->next;
+		}
+	}
+	return resultado;	
+}
+
+Tdata intersection_set(Tdata A, Tdata B) {
+	Tdata resultado;
+	Tdata aux1 = A;
+	Tdata aux2 = B;
+	Tdata fin;
+	
+	copy_set(resultado, A);
+	
+	if(!esvacia(A) && !esvacia(B) {
+		return -1;
+	}else{
+		while(aux1 != NULL) {
+			while(aux2 != NULL && belong(aux1->data, aux2->data) != 0) {
+				aux2 = aux2->next;
+			}
+			
+			nuevo = create_set();
+			copy_ser(nuevo->data, aux2->data);
+			
+			nuevo->next = resultado;
+			resultado = nuevo;
+			
+			aux1 = aux1->next;
+		}
+	}
+	return resultado;
+}
+
+Tdata difference_set(Tdata A, Tdata B) {
+	Tdata resultado;
+	Tdata aux1 = A;
+	Tdata aux2 = B;
+	Tdata fin;
+	
+	copy_set(resultado, A);
+	
+	if(!esvacia(A) && !esvacia(B) {
+		return -1;
+	}else{
+		while(aux1 != NULL) {
+			while(aux2 != NULL && belong(aux1->data, aux2->data) != 1) {
+				aux2 = aux2->next;
+			}
+			
+			nuevo = create_set();
+			nuevo->data= copia_ast(aux2->data);
+			
+			nuevo->next = resultado;
+			resultado = nuevo;
+			
+			aux1 = aux1->next;
+		}
+	}
+	return resultado;
+}
 #endif

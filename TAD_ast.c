364ef0786c2ea53582194f781aa133b6dabc8fd4
facu <150148503+chumele@users.noreply.github.com>
2026-04-30 13:02:50 -0300

    Refactor TAD_ast.c to improve structure and clarity

--- TAD_ast.c
+++ TAD_ast.c
@@ -1,16 +1,18 @@
-#include "TAD_AST.h"
+#include "TAD_ast.h"
 #include "TAD_set.h"
 #include "TAD_list.h"
+
 Tdata copia_ast(Tdata elemento);
 int esvacio(Tdata ast);
 
 Tdata create_str(){ 
-	Tdata str = (Tdata)malloc(sizeof(TNodo));
+	Tdata str = malloc(sizeof(struct dataType));
 	str->nodeType = 1;
-	
+	str->string = NULL;
 	return str;
 }
-Tdata create_set(){
+	
+Tdata create_set(){		
 	Tdata set =(Tdata)malloc(sizeof(TNodo));
 	set->nodeType = 2;
 	set->data = NULL;
@@ -24,53 +26,194 @@ Tdata create_list(){
 	list->next = NULL;
 	return list;
 }
-
-Tdata create_gen(int tipo){ // Selector de tipos de datos
+void mostrarArbol(Tdata arbol) {
+	if(arbol!=NULL){
+		switch (arbol->nodeType) {
+		case 1:
+			print(arbol->string);
+			break;
+		case 2:
+			printf("{");
+			Tdata aux1 = arbol->data;
+			while (aux1 != NULL) {
+				mostrarArbol(aux1->data);
+				aux1 = aux1->next;
+				if (aux1 != NULL) printf(",");
+			}
+			printf("}");
+			break;
+			case 3:
+				printf("[");
+				Tdata aux = arbol->data;
+				while (aux != NULL) {
+					mostrarArbol(aux->data);
+					aux = aux->next;
+					if (aux != NULL) printf(",");
+				}
+				printf("]");
+				break;
+		}
+	}else {
+		printf("\nVacio");
+	} 
+}
+int equals_node(Tdata n1, Tdata n2) { // Retorna 1 si son iguales 
+	if (n1 == NULL && n2 == NULL){ 
+		return 1;
+	}else {
+		if(n1 == NULL || n2 == NULL){
+			return 0;
+		}else { 
+			if (n1->nodeType != n2->nodeType){
+				return 0;
+			}else {
+				if (n1->nodeType == 1) { 
+					return compare_str(n1->string, n2->string); 
+				} else {
+					
+					// Comparación estructural para Listas/Conjuntos
+					if(equals_node(n1->data, n2->data) && equals_node(n1->next, n2->next)){
+						return 1;
+					}
+					return 0;
+				}
+			}
+		}
+	}
+}
+void eliminarprimero(Tdata* ast) {
+	Tdata aux = (ast)->data;
+	(ast)->data = aux->next;
+	free_ast(aux);
+}
+void free_ast(Tdata ast) {
+	if (ast == NULL) return;
+	
+	if (ast->nodeType ==1) {
+		free_str(ast->string);
+	}
+	else {
+		Tdata aux = ast->data;
+		while (aux != NULL) {
+			Tdata next = aux->next;
+			free_ast(aux);
+			aux = next;
+		}
+	}
+	free(ast);
+}
+Tdata prod_cartesiano(Tdata a, Tdata b) {
+	if (a == NULL || b == NULL || a->data == NULL || b->data == NULL) return create_list();
+	Tdata prod = create_list();
+	Tdata auxa = a->data; // Recorremos los eslabones de la lista A
+	while (auxa != NULL) {
+		Tdata auxb = b->data; // Recorremos los eslabones de la lista B
+		while (auxb != NULL) {
+			
+			// Creamos un nuevo "par": [elementoA, elementoB]
+			Tdata par = create_list();
+			append(&par, auxa->data); // Metemos el elemento de A
+			append(&par, auxb->data); // Metemos el elemento de B
+			
+			// Añadimos ese par a la lista de resultados
+			append(&prod, par);
+			auxb = auxb->next;
+		}
+		auxa = auxa->next;
+	}
+	return prod;
+}
+void append(Tdata* list, Tdata elem) {
+	
+	if (elem != NULL){	
+		Tdata nuevo = create_list();          
+		nuevo->data = copy_ast(elem);         
+		nuevo->next = NULL;
+		if ((*list)->data == NULL) {
+			(*list)->data = nuevo;
+		}else {
+			Tdata aux = (*list)->data;
+				while (aux->next != NULL) {
+					aux = aux->next;
+				}
+			aux->next = nuevo;
+		}
+	}else {
+		printf("\nLista vacia");
+	}
+}
+ // Funciones Preivadas
+Tdata copy_ast(Tdata list) { // Copia una lista y la retorna  [1,{0,1},[1]]
+	Tdata nuevo;
+	if (list == NULL) { // Verifica que la lista no este vacia
+		return NULL;
+	}
+	nuevo = NULL;
+	if (list->nodeType == 1) {
+		nuevo=create_str();
+		nuevo->string = copy_str(list->string);  
+	} else {
+		
+		// Si el elemento es una lista o conjunto entonces tiene mas elementos por lo que llamo a la misma funcion con el sig elem anidado
+		if(list->nodeType == 2){
+			nuevo=create_set();
+			nuevo->data = copy_ast(list->data); 
+			nuevo->next = copy_ast(list->next);
+		}else {
+			nuevo=create_list();
+			nuevo->data = copy_ast(list->data); 
+			nuevo->next = copy_ast(list->next);
+		}
+	}
+	return nuevo; // Retorno el nuevo arbol 
+}
+int esvacio(Tdata ast){
+	if (ast==NULL){
+		return 1; //1 si es vacio
+	}else{
+		return 0;
+	}
+}
+	// funciones de carga por consola trash
+/*Tdata create_gen(int tipo){ // Selector de tipos de datos
 	Tdata gen;
-	switch (tipo) {
-	case 1:
-		gen=create_str();
+		switch (tipo) {
+			case 1:
+			gen=create_str();
 		break;
-	case 2:
-		gen=create_set();
+		case 2:
+			gen=create_set();
 		break;
-	case 3:
-		gen=create_list();
+		case 3:
+			gen=create_list();
 		break;
 	}
 	return gen;
 }
-int esvacio(Tdata ast){
-	if (ast==NULL){
-		return 0;
-	}else{
-		return 1;
-	}
-}
-	Tdata create_ast(){
-		Tdata data;
-		int tipo;
-		do{ // verifica que se esten ingresando numeros dentro del rango solicitado entre 1 y 3
-			printf("\nIngrese el tipo de dato que quiere ingresar: [1] str, [2] set y [3] list");
-			scanf("%d",&tipo);
-		} while(tipo < 1 || tipo > 3);
+Tdata create_ast(){
+	Tdata data;
+	int tipo;
+	do{
+		// verifica que se esten ingresando numeros dentro del rango solicitado entre 1 y 3
+		printf("\nIngrese el tipo de dato que quiere ingresar: [1] str, [2] set y [3] list");
+		scanf("%d",&tipo);
 		
-		data = create_gen(tipo); // Devuelve el dato correspondiente.
-	
-		switch(tipo) {
-		case 1:
+	} while(tipo < 1 || tipo > 3);
+	data = create_gen(tipo); // Devuelve el dato correspondiente.
+	switch(tipo) {
+	case 1:
 		data->string = load(); // solo strings
 		break;
 		case 2:
 		data->data = cargar_conjunto(); // o lo que corresponda
 		break;
-		case 3:
-		data->data = cargar_lista();
+	case 3:
+		data->data = carga_lista();
 		break;
-		}
-		data->next = NULL; 
-		return data;
 	}
+	data->next = NULL; 
+	return data;
+}
 Tdata cargar_conjunto(){
 	Tdata arbol;
 	arbol=create_set();
@@ -85,52 +228,6 @@ Tdata cargar_conjunto(){
 	} while(opcion!=0);
 	return arbol;
 }
-	int equals_node(Tdata n1, Tdata n2) { //retorna 1 si son iguales 
-		if (n1 == NULL && n2 == NULL) 
-			return 1;
-		else {
-			if(n1 == NULL || n2 == NULL)
-				return 0;
-			else { 
-				if (n1->nodeType != n2->nodeType) 
-					return 0;
-				else {
-					if (n1->nodeType == 1) { 
-						return compare_str(n1->string, n2->string); 
-					} else { // Comparaci�n estructural para Listas/Conjuntos
-						return equals_node(n1->data, n2->data) && equals_node(n1->next, n2->next);
-					}
-				}
-			}
-		}
-	}
-	Tdata copia_ast(Tdata elemento){  //[1,{0,1},[1]]
-		Tdata ast;
-		if(!esvacio(elemento)){
-			ast=(Tdata)malloc(sizeof(TNodo));
-			ast->nodeType=elemento->nodeType;// numero que representa el tipo de dato a almacenar 
-			switch(elemento->nodeType){
-			case 1: ast->string=copy_str(elemento->string); break;
-			case 2: //ast->data=copia_conjunto(elemento>data); break;
-			case 3: cop break;
-			}
-			return ast;
-		}
-		else{
-			return NULL;
-		}
-	}
-
-void eliminarprimero(Tdata* set){
-	Tdata aux;
-	if(*set!=NULL){
-		aux=*set;
-		(*set)=(*set)->next;
-		free(aux);
-	}
-}
-
-
 Tdata carga_lista(){
 	Tdata arbol;
 	arbol=create_list();
@@ -144,50 +241,5 @@ Tdata carga_lista(){
 		scanf("%d",&opcion);
 	} while(opcion!=0);
 	return arbol;
-}
-void mostrarArbol(Tdata arbol) {
-	if (arbol == NULL) return;
-	
-	switch (arbol->nodeType) {
-	case 1:
-		print(arbol->string);
-		break;
-		
-	case 2:
-		printf("{");
-		while (arbol != NULL && arbol->nodeType == 2) {
-			mostrarArbol(arbol->data);
-			arbol = arbol->next;
-			if (arbol != NULL)
-				printf(",");
-		}
-		printf("}");
-		break;
-		
-		case 3:
-			printf("[");
-			while (arbol != NULL && arbol->nodeType == 3) {
-				mostrarArbol(arbol->data);
-				arbol = arbol->next;
-				if (arbol != NULL)
-					printf(",");
-			}
-			printf("]");
-			break;
-	}
-} 
-Tdata prod_cartesiano(Tdata a, Tdata b){
-	Tdata prod,aux;
-	prod=create_list();
-	Tdata nuevo;
-	aux=a;
-	while(a!=NULL){
-		while(b!=NULL){
-			nuevo->data=copy_list(a->data);
-			nuevo->data->next=copy_list(b->data);
-			append(&prod,nuevo);
-		}
-	}
-	return prod;	
-}
+}*/
 

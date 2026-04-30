db2a2e0e944cbac8c4dd64ea6ab78acde8c3bc0c
facu <150148503+chumele@users.noreply.github.com>
2026-04-30 15:56:57 -0300

    Update TAD_set.h

--- TAD_set.h
+++ TAD_set.h
@@ -2,14 +2,14 @@
 #define TAD_SET_H
 
  // Operaciones sobre SET
-void insert_set(Tdata* set, Tdata elem); // Pasa el conjunto por referencia y se inserta un nuevo elemento (sin duplicados)
-void remove_set(Tdata* set, Tdata elem); // Elimina un elemento del conjunto
-int belongs(Tdata set, Tdata elem); // Retorna 0 si no pertenece - 1 si pertenece el elemento al conjunto
+void insert_set(Tdata* set, Tdata elemento); // Pasa el conjunto por referencia y se inserta un nuevo elemento (sin duplicados)
+void remove_set(Tdata* set, Tdata elemento); // Elimina un elemento del conjunto
+int belongs(Tdata set, Tdata elemento); // Retorna 0 si no pertenece - 1 si pertenece el elemento al conjunto
  // Operaciones algebraicas
-Tdata union_set(Tdata A, Tdata B); // Retorna la union de dos conjuntos
-Tdata intersection_set(Tdata A, Tdata B); // Retorna la interseccion de dos conjuntos
-Tdata difference_set(Tdata A, Tdata B); // Retorna la diferencia de dos conjuntos
-int subset(Tdata A, Tdata B);// Retorna 1 si es subconjunto - 0 si no lo es
-int equals_set(Tdata A, Tdata B); // Retorna 1 si son iguales - 0 si no lo son
+Tdata union_set(Tdata set1, Tdata set2); // Retorna la union de dos conjuntos
+Tdata intersection_set(Tdata set1, Tdata set2); // Retorna la interseccion de dos conjuntos
+Tdata difference_set(Tdata set1, Tdata set2); // Retorna la diferencia de dos conjuntos
+int subset(Tdata set1, Tdata set2);// Retorna 1 si es subconjunto - 0 si no lo es
+int equals_set(Tdata set1, Tdata set2); // Retorna 1 si son iguales - 0 si no lo son
 
 #endif

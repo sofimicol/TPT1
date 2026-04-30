e618bffdf029d8601d17acd01f17767d1341eaee
facu <150148503+chumele@users.noreply.github.com>
2026-04-30 15:53:03 -0300

    Update TAD_list.c

--- TAD_list.c
+++ TAD_list.c
@@ -1,10 +1,12 @@
 #include "TAD_AST.h"
 #include "TAD_list.h"
-void append(Tdata* list, Tdata elem) { //cargar una lista que dentro puede tener datos str, set o list
+
+ // Cargar una lista que dentro puede tener datos str, set o list
+void append(Tdata* list, Tdata elem) {
 	Tdata nuevo; 
-	nuevo=copia_ast(elem);
+	nuevo = copia_ast(elem);
 	if (estavacio(*list)) {
-		(*list) = nuevo; //primer elemento
+		(*list) = nuevo; // Primer elemento
 	}else {
 		Tdata aux = *list;
 		while (aux->next != NULL) { // Desplazamiento para obtener posicion del anteultimo nodo
@@ -15,24 +17,22 @@ void append(Tdata* list, Tdata elem) { //cargar una lista que dentro puede tener
 }
 int length(Tdata list) { // Calcula el tamanio de lalista
 	int c = 0; // Contador
-	Tdata aux = list; // Auxiliar (opcional) para despalzarce
-	
-	while(aux != NULL) { // Desplazamiento
+	Tdata aux = list;
+	while(aux != NULL) {
 		c++;
 		aux = aux->next;
 	}
-	
-	return c; // Tamanio de la lista
+	return c; // Retorno tamanio de lista
 }
-//posible ast
-Tdata copy_list(Tdata list) {//Copia una lista y la retorna  [1,{0,1},[1]]
+ // Copia una lista y la retorna  [1,{0,1},[1]]
+Tdata copy_list(Tdata list) {
 	Tdata nuevo;
 	if (list == NULL) { // Verifica que la lista no este vacia
 		return NULL;
 	}
 	if (list->nodeType == 1) {
 		nuevo->string = copy_str(list->string);  
-	} else { //Si el elemento es una lista o conjunto entonces tiene mas elementos por lo que llamo a la misma funcion con el sig elem anidado
+	} else { // Si el elemento es una lista o conjunto entonces tiene mas elementos por lo que llamo a la misma funcion con el sig elem anidado
 		if(list->nodeType == 2){
 			nuevo=create_set();
 			nuevo->data = copy_list(list->data); 
@@ -44,46 +44,32 @@ Tdata copy_list(Tdata list) {//Copia una lista y la retorna  [1,{0,1},[1]]
 			nuevo->next = copy_list(list->next);
 		}
 	}
-	return nuevo; //Retorno el nuevo arbol 
+	return nuevo; // Retorno el nuevo arbol 
 }
-Tdata concatena_list(Tdata L1, Tdata L2) { 
-	Tdata resultado = creat_list(); // Declaro set
-	Tdata aux;
-	Tdata nuevo;
-	if(esvacio(L1) == 1 && esvacio(L2) == 1) {
-		resultado = copia_ast(L1);
-		aux = L2;
-		
-		while(L2 != NULL) {
-			nuevo = creat_list();
-			nuevo->data = copia_ast(L2, 2); //{0,1}
-			append(&nuevo, L2);
-			L2 = L2->next;
-		}
-		
-	}else {
-		if(esvacio(L1) == 1) {
-			resultado = copia_ast(L1);
-			
-		}else {
-			if(esvacio(L2) == 1){
-				resultado = copia_ast(L2);
-			}
-			else{
-				resultado=NULL;
-			}
-		}
-	}
-	return resultado;
+Tdata concat(Tdata L1, Tdata L2) {
+    Tdata resultado = create_list();
+    Tdata aux;
+	 // Copio los elementos de L1 a resultado
+    aux = L1;
+    while(aux != NULL){
+        append(&resultado, aux->data);
+        aux = aux->next;
+    }
+    aux = L2; 
+	 // Copio los elementos de L2 al final de resultado
+    while(aux != NULL){
+        append(&resultado, aux->data);
+        aux = aux->next;
+    }
+    return resultado;
 }
-
-int search(Tdata list, Tdata elem){
+int search(Tdata list, Tdata elem) {
 	Tdata aux = copy_list(list);
 	while (aux != NULL) {
-		if (equals_node(aux->data, elem)==1) { //Comparo los nodos
+		if (equals_node(aux->data, elem) == 1) { // Comparo los nodos
 			return 1; // El elemento se ecnontro en la lista
 		}
-		aux = aux->next; //Contuinua el ciclo hasta que se termine la lista
+		aux = aux->next; // Contuinua el ciclo hasta que se termine la lista
 	}
-	return 0; //El elemento no se encontro 
+	return 0; // El elemento no se encontro 
 }

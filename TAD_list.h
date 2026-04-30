80103315101d88a640344c2777168878aea28767
facu <150148503+chumele@users.noreply.github.com>
2026-04-30 15:53:40 -0300

    Update TAD_list.h

--- TAD_list.h
+++ TAD_list.h
@@ -3,10 +3,11 @@
 
 typedef struct dataType* Tdata;
 
+ // Operaciones sobre LIST
 void append(Tdata* list, Tdata elem);
 int length(Tdata list);
 Tdata copy_list(Tdata list);
-Tdata concatena_list(Tdata list1, Tdata list2);
+Tdata concat(Tdata list1, Tdata list2);
 int search(Tdata list, Tdata elem);
 
 #endif
